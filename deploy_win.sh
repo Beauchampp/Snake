278c0b0e296dec4c243507b4edbe53468f321c13
DE BACKER Thibald <thibald.debacker.TDB@gmail.com>
2015-12-17 17:00:12 +0100

    Ajout du deploy win

---
+++ deploy_win.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+application_path="./Snake/resources/app/"
+
+cp ./package.json $application_path
+cp ./main.js $application_path
+cp ./index.html $application_path
+cp -R ./assets $application_path
