ba7a190694214bc12c82f0911154668dedb5b0a0
Beauchamp Mignon <mignon.beauchamp@gmail.com>
2015-12-17 11:00:05 +0100

    Corrige le js

--- assets/js/game_over.js
+++ assets/js/game_over.js
@@ -1,16 +1,17 @@
 var Game_Over = {
 
     preload : function() {
-        // Load the needed image for this game screen.
+        // Here we load all the needed resources for the level.
+        // In our case, that's just two squares - one for the snake body and one for the apple.
         game.load.image('gameover', './assets/images/gameover.png');
     },
 
     create : function() {
 
-        // Create button to start game like in Menu.
+        // Create button to start game similar to the main menu.
         this.add.button(0, 0, 'gameover', this.startGame, this);
 
-        // Add text with information about the score from last game.
+        // Last Score Info.
         game.add.text(235, 350, "LAST SCORE", { font: "bold 16px sans-serif", fill: "#46c0f9", align: "center"});
         game.add.text(350, 348, score.toString(), { font: "bold 20px sans-serif", fill: "#fff", align: "center" });
 
@@ -18,7 +19,7 @@ var Game_Over = {
 
     startGame: function () {
 
-        // Change the state back to Game.
+        // Change the state to the actual game.
         this.state.start('Game');
 
     }
